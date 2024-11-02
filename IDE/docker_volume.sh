7c14453d96a427a32724c23d4551a9946e65a269
kiljw316 <kiljw316@gmail.com>
2024-11-02 17:54:58 +0900

    test

--- IDE/docker_volume.sh
+++ IDE/docker_volume.sh
@@ -1,7 +1,7 @@
 # 볼륨 새성 (volume name : cloud-cicd-src)
 docker volume create --opt device="C:/Users/CICD/Desktop/cicd/cicd-start/IDE/src" --opt o=bind --opt type=none cloud-cicd-src
-docker volume create --opt device="C:/Users/CICD/Desktop/cicd/cicd-start/IDE/src/vscode" --opt o=bind --opt type=none vscode
-docker volume create --opt device="C:/Users/CICD/Desktop/cicd/cicd-start/IDE/src/vscode/jenkins" --opt o=bind --opt type=none jenkins-home
+docker volume create --opt device="C:/Users/CICD/Desktop/cicd/cicd-start/IDE/vscode" --opt o=bind --opt type=none vscode
+docker volume create --opt device="C:/Users/CICD/Desktop/cicd/cicd-start/IDE/jenkins" --opt o=bind --opt type=none jenkins-home
 
 # 볼륨 생성 확인
 docker volume ls
